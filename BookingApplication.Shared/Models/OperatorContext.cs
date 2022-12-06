cf9fcf0611bc8525be07971c222b61ea2f89a79f
Saad Bilal <saad103bilal@gmail.com>
2022-12-06 03:37:27 +0300

    Summary component created

--- BookingApplication.Shared/Models/OperatorContext.cs
+++ BookingApplication.Shared/Models/OperatorContext.cs
@@ -6,6 +6,7 @@ public class OperatorContext
     public string PrimaryTextColor { get; set; }
     public string SecondaryColor { get; set; }
     public string SecondaryTextColor { get; set; }
+    public string AccentColor { get; set; }
     public string CompanyPrimaryPhone { get; set; }
     public string CompanySecondaryPhone { get; set; }
     public string CompanyEmail { get; set; }
